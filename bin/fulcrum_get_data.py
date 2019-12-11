680c3f7105f000e744e96b4710dcd922714875d5
Satoshi Kawasaki <hobbes3@gmail.com>
2019-12-11 22:56:21 +0000

    Updated code for new splunk_rest.

--- bin/fulcrum_get_data.py
+++ bin/fulcrum_get_data.py
@@ -8,7 +8,7 @@ from splunk_rest.splunk_rest import splunk_rest, try_response
 
 def get_form(form_id):
     @try_response
-    def send_form(r):
+    def send_form(r, *args, **kwargs):
         data = ""
 
         records = r.json()["records"]
